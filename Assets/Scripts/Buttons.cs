0742e9ff9b02178a4a0d89c78b796f4df2b712c8
Matthew Nepywoda <ma907122@ucf.edu>
2025-04-21 01:35:15 -0400

    title screen
    added settings (volume slider) to title screen, finalized sprites to character select. other small changes

--- Assets/Scripts/Buttons.cs
+++ Assets/Scripts/Buttons.cs
@@ -7,14 +7,37 @@ using UnityEngine.UI;
 public class Buttons : MonoBehaviour
 {
     public Sprite[] charParts;
+    public Slider volumeSlider;
     public GlobalManager globalManager;
     public GameObject characterSelect;
+    public GameObject settingSelect;
     public Image currentSprite;
-    public int i = 0;
+
+    public Image currentVol;
+    public Sprite volumeHigh;
+    public Sprite volLow;
+    public Sprite volMute;
+    private int i = 0;
+
+    void Start()
+    {
+        if(!PlayerPrefs.HasKey("musicVolume"))
+        {
+            PlayerPrefs.SetFloat("musicVolume", 1);
+            LoadSettings();
+        }
+        else
+            LoadSettings();
+    }
 
     public void CharacterSelect()
     {
         characterSelect.SetActive(true);
+    }
+
+    public void SettingSelect()
+    {
+        settingSelect.SetActive(true);
     }
 
     public void ExitCharacterSelect()
@@ -22,11 +45,50 @@ public class Buttons : MonoBehaviour
         characterSelect.SetActive(false);
     }
 
+    public void ExitSettings()
+    {
+        settingSelect.SetActive(false);
+    }
+
     public void StartGame()
     {
         SceneManager.LoadScene("SampleScene");
     }
 
+    public void ChangeVolume()
+    {
+        AudioListener.volume = volumeSlider.value;
+        SaveSettings();
+        UpdateVolumeButton();
+    }
+
+    public void Mute()
+    {
+        if(AudioListener.volume == 0)
+            AudioListener.volume = volumeSlider.value;
+        else
+            AudioListener.volume = 0;
+        UpdateVolumeButton();
+    }
+    public void UpdateVolumeButton()
+    {
+        if(AudioListener.volume == 0)
+            currentVol.sprite = volMute;
+        else if(AudioListener.volume <= 0.4)
+            currentVol.sprite = volLow;
+        else
+            currentVol.sprite = volumeHigh;
+    }
+
+    public void LoadSettings()
+    {
+        volumeSlider.value = PlayerPrefs.GetFloat("musicVolume");
+    }
+
+    public void SaveSettings()
+    {
+        PlayerPrefs.SetFloat("musicVolume", volumeSlider.value);
+    }
     public void ChangeCharacterAdd()
     {   
         i++;    
